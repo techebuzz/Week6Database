ab17335f7a561e079c4813adfe2ad99b560863a8
Gerald Kanjatechebuzz <kanjagerald717@gmail.com>
2025-04-29 20:13:48 +0300

    initial commit

---
+++ answers.sql
@@ -0,0 +1,34 @@
+ USE salesdb;
+ 
+-- Employee and Office Information
+SELECT 
+    e.firstName,
+    e.lastName,
+    e.email,
+    e.officeCode
+FROM 
+    employees e
+INNER JOIN 
+    offices o ON e.officeCode = o.officeCode;
+
+-- Product Information with Product Lines
+SELECT 
+    p.productName,
+    p.productVendor,
+    p.productLine
+FROM 
+    products p
+LEFT JOIN 
+    productlines pl ON p.productLine = pl.productLine;
+
+-- Order Information with Customer Details
+SELECT 
+    o.orderDate,
+    o.shippedDate,
+    o.status,
+    o.customerNumber
+FROM 
+    customers c
+RIGHT JOIN 
+    orders o ON c.customerNumber = o.customerNumber
+LIMIT 10;
